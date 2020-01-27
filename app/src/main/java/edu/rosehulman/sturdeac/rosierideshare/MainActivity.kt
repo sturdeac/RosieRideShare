62570c3ee584726d937ce4f65834ac3129cf7e51
Adam Stephenson <stepheac@stepheac-5510.rose-hulman.edu>
2020-01-26 19:02:07 -0500

    MapBox User Location

--- app/src/main/java/edu/rosehulman/sturdeac/rosierideshare/MainActivity.kt
+++ app/src/main/java/edu/rosehulman/sturdeac/rosierideshare/MainActivity.kt
@@ -1,23 +1,68 @@
 package edu.rosehulman.sturdeac.rosierideshare
 
+import android.app.Activity
+import android.content.Intent
+import android.location.Location
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.mapbox.android.core.location.LocationEngine
+import com.mapbox.android.core.location.LocationEngineListener
+import com.mapbox.android.core.location.LocationEnginePriority
+import com.mapbox.android.core.location.LocationEngineProvider
+import com.mapbox.android.core.permissions.PermissionsListener
+import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraUpdate
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.location.LocationComponent
+import com.mapbox.mapboxsdk.location.modes.CameraMode
+import com.mapbox.mapboxsdk.maps.MapboxMap
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), PermissionsListener, LocationEngineListener, OnMapReadyCallback {
+
+    val REQUEST_CHECK_SETTINGS = 1
+    var settingsClient: SettingsClient? = null
+
+    lateinit var map: MapboxMap
+    lateinit var permissionManager: PermissionsManager
+    var originLocation: Location? = null
+    var locationEngine: LocationEngine? = null
+    var locationComponent: LocationComponent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         Mapbox.getInstance(this, "pk.eyJ1Ijoic3R1cmRlYWMiLCJhIjoiY2ppZWx2OWtyMDgwbDNrcXBhbnA5cG84OCJ9.Hp5qq-G8KXwAW4hUjP6QVg");
         setContentView(R.layout.activity_main)
+        mapbox.getMapAsync(this)
+        settingsClient = LocationServices.getSettingsClient(this)
         setSupportActionBar(toolbar)
         mapbox.onCreate(savedInstanceState)
 
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CHECK_SETTINGS) {
+            if (resultCode == Activity.RESULT_OK) {
+                enableLocation()
+            } else if (resultCode == Activity.RESULT_CANCELED) {
+                finish()
+            }
+        }
+    }
+
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -40,6 +85,11 @@ class MainActivity : AppCompatActivity() {
     @SuppressWarnings("MissingPermission")
     override fun onStart() {
         super.onStart()
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            locationEngine?.requestLocationUpdates()
+            locationComponent?.onStart()
+        }
+
         mapbox.onStart()
     }
 
@@ -55,17 +105,122 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
+        locationEngine?.removeLocationUpdates()
+        locationComponent?.onStop()
         mapbox.onStop()
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        locationEngine?.deactivate()
         mapbox.onDestroy()
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
         mapbox.onLowMemory()
+    }
+
+    override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
+        Toast.makeText(this, "This app needs location permission to be able to show your location on the map", Toast.LENGTH_LONG).show()
+
+    }
+
+    override fun onPermissionResult(granted: Boolean) {
+        if (granted) {
+            enableLocation()
+        } else {
+            Toast.makeText(this, "User location was not granted", Toast.LENGTH_LONG).show()
+            finish()
+        }
+
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        location?.run {
+            originLocation = this
+            setCameraPosition(this)
+        }
+
+    }
+
+    override fun onConnected() {
+        locationEngine?.requestLocationUpdates()
+    }
+
+    override fun onMapReady(mapboxMap: MapboxMap?) {
+        //1
+        map = mapboxMap ?: return
+
+        val locationRequestBuilder = LocationSettingsRequest.Builder().addLocationRequest(LocationRequest()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY))
+
+        val locationRequest = locationRequestBuilder?.build()
+
+        settingsClient?.checkLocationSettings(locationRequest)?.run {
+            addOnSuccessListener {
+                enableLocation()
+            }
+
+            addOnFailureListener {
+                val statusCode = (it as ApiException).statusCode
+
+                if (statusCode == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
+                    val resolvableException = it as? ResolvableApiException
+                    resolvableException?.startResolutionForResult(this@MainActivity, REQUEST_CHECK_SETTINGS)
+                }
+            }
+        }
+
+    }
+
+    override fun onRequestPermissionsResult( requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        permissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    fun enableLocation(){
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            initializeLocationComponent()
+            initializeLocationEngine()
+        } else {
+            permissionManager = PermissionsManager(this)
+            permissionManager.requestLocationPermissions(this)
+        }
+
+    }
+
+    @SuppressWarnings("MissingPermission")
+    fun initializeLocationEngine() {
+        locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
+        locationEngine?.priority = LocationEnginePriority.HIGH_ACCURACY
+        locationEngine?.activate()
+        locationEngine?.addLocationEngineListener(this)
+
+        val lastLocation = locationEngine?.lastLocation
+        if (lastLocation != null) {
+            originLocation = lastLocation
+            setCameraPosition(lastLocation)
+        } else {
+            locationEngine?.addLocationEngineListener(this)
+        }
+
+    }
+
+    @SuppressWarnings("MissingPermission")
+    fun initializeLocationComponent() {
+        locationComponent = map.locationComponent
+        locationComponent?.activateLocationComponent(this)
+        locationComponent?.isLocationComponentEnabled = true
+        locationComponent?.cameraMode = CameraMode.TRACKING
+
+    }
+
+    fun setCameraPosition(location: Location){
+        map.animateCamera(
+            CameraUpdateFactory.newLatLng(LatLng(location.latitude, location.longitude))
+        )
+        map.animateCamera(CameraUpdateFactory.zoomTo(22.0))
+
     }
 
     // end mapbox
